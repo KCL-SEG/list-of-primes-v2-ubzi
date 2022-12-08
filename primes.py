029d4a8342f3072cc4763d5ac9b36d892fda2174
ubzi <115138285+ubzi@users.noreply.github.com>
2022-12-08 22:13:36 +0000

    Update primes.py

--- primes.py
+++ primes.py
@@ -2,5 +2,19 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if number_of_primes < 0:
+        raise ValueError('Number must be positive')
     list = []
+    counter = 2
+    while len(list) < number_of_primes:
+        if PrimeCheck(counter) == True:
+            list.append(counter)
+        counter += 1
     return list
+
+
+def PrimeCheck(num):
+    for i in range (2,(num // 2) + 1 ):
+        if (num % i) == 0:
+            return False
+    return True
